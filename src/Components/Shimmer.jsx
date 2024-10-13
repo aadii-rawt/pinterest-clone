45f9315cb5ee96c74c12e64e81ebde35194e468a
adityarawat4 <rawatadii060@gmail.com>
2024-10-13 20:55:13 +0530

    c

---
+++ src/Components/Shimmer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Masonry from 'react-masonry-css';
+
+function Shimmer() {
+
+    const breakpointColumnsObj = {
+        default: 4,
+        1100: 3,
+        700: 2,
+        500: 2
+    };
+
+    return (
+        <div>
+            <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+            >
+                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-60 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-60 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-60 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-60 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+            </Masonry>
+
+        </div>
+    )
+}
+
+export default Shimmer
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,17 +6,18 @@ import { useData } from '../Context/DataProvider';
 import Post from '../Components/Post';
 // import { fakePins, users } from '../utils'
 import Masonry from 'react-masonry-css';
+import Shimmer from '../Components/Shimmer';
 function Home() {
   const [pins, setPins] = useState([])
-  const {fakePins,users} = useData()
+  const { fakePins, users } = useData()
   const [lastDoc, setLastDoc] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [hasMore, setHasMore] = useState(true);
 
   // useEffect(()=>{
   //   const pinsData = fakePins.map((data) => {
   //     const userData = users.find((d) => d?.userId === data?.createdBy)
-      
+
   //     return {...userData, ...data} 
   //   })
   //   setPins(pinsData)
@@ -27,28 +28,34 @@ function Home() {
     const fetchPosts = async () => {
       const postsCollection = collection(db, "posts"); // Replace 'posts' with your collection name
       const postsSnapshot = await getDocs(postsCollection);
-      const postList = await Promise.all(
-        postsSnapshot.docs.map(async (postDoc) => {
-          const postData = postDoc.data();
-          const userId = postData?.createdBy; // Assuming post contains userId
-
-          // Fetch user details from 'users' collection using userId
-          const userDocRef = doc(db, "users", userId); // Correctly creating a reference to the user's document
-          const userDoc = await getDoc(userDocRef); // Correct usage of getDoc to fetch user data
-          const userData = userDoc.exists() ? userDoc.data() : null;
-
-          return {
-            id: postDoc.id,
-            ...postData,
-            user: userData, // Attach user details to the post
-          };
-        })
-      );
-      setPins(postList);
+      try {      
+        const postList = await Promise.all(
+          postsSnapshot.docs.map(async (postDoc) => {
+            const postData = postDoc.data();
+            const userId = postData?.createdBy; // Assuming post contains userId
+  
+            // Fetch user details from 'users' collection using userId
+            const userDocRef = doc(db, "users", userId); // Correctly creating a reference to the user's document
+            const userDoc = await getDoc(userDocRef); // Correct usage of getDoc to fetch user data
+            const userData = userDoc.exists() ? userDoc.data() : null;
+  
+            return {
+              id: postDoc.id,
+              ...postData,
+              user: userData, // Attach user details to the post
+            };
+          })
+        );
+        setPins(postList);
+        setLoading(false)
+      } catch (error) {
+        console.log("this is error", error);
+        
+      }
     };
 
     console.log(pins);
-    
+
 
     fetchPosts();
   }, []);
@@ -58,8 +65,13 @@ function Home() {
     default: 4,
     1100: 3,
     700: 2,
-    500:2
+    500: 2
   };
+
+  if (loading) {
+    return <Shimmer />
+  }
+
 
   return (
     <Masonry
@@ -67,7 +79,7 @@ function Home() {
       className="my-masonry-grid"
       columnClassName="my-masonry-grid_column"
     >
-      {pins?.map((data,index) => (
+      {pins?.map((data, index) => (
         <Post key={index} data={data} showUserDetails={true} />
       ))
       }

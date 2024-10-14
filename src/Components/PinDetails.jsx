e7717a22dc4b7e0ebe33b7e863fcbc07a916d704
adityarawat4 <rawatadii060@gmail.com>
2024-10-14 23:54:13 +0530

    c

--- src/Components/PinDetails.jsx
+++ src/Components/PinDetails.jsx
@@ -4,13 +4,13 @@ import CommentList from './CommentList';
 import { Link } from 'react-router-dom';
 import { useData } from '../Context/DataProvider';
 
-function PinDetails({ id, pin }) {
-    const [userDetails, setUserDetails] = useState(null)
+function PinDetails({ id, pin,user }) {
+    // const [userDetails, setUserDetails] = useState(null)
     const {users} = useData()
-    useEffect(() => {
-        const userData = users?.find((d) => d?.userId === pin?.createdBy)
-        setUserDetails(userData)
-    }, [pin])
+    // useEffect(() => {
+    //     const userData = users?.find((d) => d?.userId === pin?.createdBy)
+    //     setUserDetails(userData)
+    // }, [pin])
 
     return (
         <div className='pindetails py-3 h-full'>
@@ -19,13 +19,13 @@ function PinDetails({ id, pin }) {
             <p>{pin?.link}</p>
             <p>{pin?.tags}</p>
             <div className='flex justify-between items-center'>
-                <Link to={`/user/{${userDetails?.userId}}`} className='flex items-center gap-2'>
-                    {userDetails?.avatar ?
-                        <img src={`/${userDetails?.avatar}`} alt="" className='w-8 h-8 rounded-full' />
+                <Link to={`/user/{${user?.username}}`} className='flex items-center gap-2'>
+                    {user?.avatar ?
+                        <img src={`/${user?.avatar}`} alt="" className='w-8 h-8 rounded-full' />
                         :
-                        <div className='w-8 h-8 rounded-full capitalize bg-red-400 flex items-center justify-center text-white'>{userDetails?.username?.[0]}</div>
+                        <div className='w-8 h-8 rounded-full capitalize bg-red-400 flex items-center justify-center text-white'>{user?.username?.[0]}</div>
                     }
-                    <p className='text-sm'>{userDetails?.username}</p>
+                    <p className='text-sm font-medium'>{user?.username}</p>
                 </Link>
                 <div><button className='btn border border-gray-700 md:px-3 md:py-1.5'>Follow</button></div>
             </div>

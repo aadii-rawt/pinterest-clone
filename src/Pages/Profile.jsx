f54ccf6792ef30d0d0eb09c92aea4210dbec6117
adityarawat4 <rawatadii060@gmail.com>
2024-10-14 23:26:54 +0530

    c

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -8,11 +8,14 @@ import { useData } from '../Context/DataProvider';
 import EditAvatar from '../Components/EditAvatar';
 import Masonry from 'react-masonry-css';
 import { Link } from 'react-router-dom';
+import Banner from '../../public/img6.jpg'
+import FollowersModal from '../Components/FollowersModal';
 
 function Profile() {
   const { user, users, fakePins } = useData()
   const [createdPosts, setCreatedPost] = useState([])
   const [isEditAvatarOpen, setIsEditAvatarOpen] = useState(false)
+  const [isFollowerOpen, setIsFollowerOpen] = useState(false)
   const [pins, setPins] = useState([])
 
   useEffect(() => {
@@ -54,6 +57,10 @@ function Profile() {
   return (
     <div className='py-5 relative'>
       <div className='profile flex justify-center items-center flex-col ' >
+        <div className=' border-2 rounded-xl' style={{"backgroundImage": `url('/img6.jpg')`}}>
+     {/* <img src="" style={{backgroundImage: "url('/img5.jpg')"}} alt="" /> */}
+        </div>
+        
         {user?.avatar ?
           <div className='flex items-center justify-center bg-green-300 w-24 h-24 cursor-pointer rounded-full' onClick={() => setIsEditAvatarOpen(true)}>
             <img src={user?.avatar} alt="" className='rounded-full w-full h-full' />
@@ -65,7 +72,7 @@ function Profile() {
         }
         {/* <div className='w-24 h-24 bg-grayTheme rounded-full'></div> */}
         <h1 className='my-2 font-semibold text-4xl capitalize'>{user?.username}</h1>
-        <h1 className='font-semibold text-base'>{user?.following?.length} Following</h1>
+        <h1 className='font-semibold text-base cursor-pointer' onClick={() => setIsFollowerOpen(true)}>{user?.following?.length} 5 Following</h1>
       </div>
 
       <div className='my-5'>
@@ -88,8 +95,7 @@ function Profile() {
 
       </div>
 
-
-
+      {isFollowerOpen && <FollowersModal setIsFollowerOpen={setIsFollowerOpen} />}
       {isEditAvatarOpen && <EditAvatar setIsEditAvatarOpen={setIsEditAvatarOpen} />}
 
     </div>

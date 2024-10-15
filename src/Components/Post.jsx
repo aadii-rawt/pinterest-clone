f5876d6e5e15f183e30c23e3f35bdac7e63efd3e
adityarawat4 <rawatadii060@gmail.com>
2024-10-15 21:51:28 +0530

    c

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 function Post({ data ,showUserDetails}) {
-console.log(data);
 
   return (
     <Link to={`/pin/${data?.id}`} className='inline-block mb-3'>

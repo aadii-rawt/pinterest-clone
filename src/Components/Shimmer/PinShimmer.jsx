9029f32dfb748b4d5e21abbf6f59fc0b6cd28aad
adityarawat4 <rawatadii060@gmail.com>
2024-10-31 18:05:44 +0530

    c

--- src/Components/Shimmer/PinShimmer.jsx
+++ src/Components/Shimmer/PinShimmer.jsx
@@ -17,15 +17,15 @@ function PinShimmer() {
                 className="my-masonry-grid"
                 columnClassName="my-masonry-grid_column"
             >
-                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
-                <div className='bg-gray-200 h-60 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
-                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
-                <div className='bg-gray-200 h-60 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
-                <div className='bg-gray-200 h-60 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
-                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
-                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
-                <div className='bg-gray-200 h-96 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
-                <div className='bg-gray-200 h-60 rounded-xl w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-96 rounded-xl md:w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-60 rounded-xl md:w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-96 rounded-xl md:w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-60 rounded-xl md:w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-60 rounded-xl md:w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-96 rounded-xl md:w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-96 rounded-xl md:w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-96 rounded-xl md:w-60 mb-3 animate-pulse duration-700'></div>
+                <div className='bg-gray-200 h-60 rounded-xl md:w-60 mb-3 animate-pulse duration-700'></div>
             </Masonry>
 
         </div>
